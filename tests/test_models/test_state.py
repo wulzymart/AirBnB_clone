48465cdc07be3fcef47c2db03760704c240c6930
adetunji <adekunleadetunjiwilson@gmail.com>
2023-08-12 13:39:58 +0100

    import the file storage module in all test files

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -6,6 +6,7 @@ This module contains the various test cases for the base module
 
 import datetime
 from models import base_model
+from models import storage
 from models.amenity import Amenity
 from models.base_model import BaseModel
 from models.city import City
@@ -13,6 +14,7 @@ from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
+from models.engine.file_storage import FileStorage
 import os
 from time import sleep
 import unittest
