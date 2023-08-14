8119fecc26f831cafc4aa8bcd9dd3b0b538102bd
adetunji <adekunleadetunjiwilson@gmail.com>
2023-08-14 05:37:01 +0100

    added test for place object attributes

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -98,6 +98,21 @@ class PlaceTest(unittest.TestCase):
         with self.assertRaises(TypeError):
             b.save(None)
 
+    def test_place_attr(self):
+        """test the place obj attributes"""
+        obj = Place()
+        self.assertEqual(obj.city_id, "")
+        self.assertEqual(obj.user_id, "")
+        self.assertEqual(obj.name, "")
+        self.assertEqual(obj.description, "")
+        self.assertEqual(obj.number_rooms, 0)
+        self.assertEqual(obj.number_bathrooms, 0)
+        self.assertEqual(obj.max_guest, 0)
+        self.assertEqual(obj.price_by_night, 0)
+        self.assertEqual(obj.latitude, 0.0)
+        self.assertEqual(obj.longitude, 0.0)
+        self.assertEqual(obj.amenity_ids, [])
+
     def tearDown(self):
         """Unset variables"""
         try:
